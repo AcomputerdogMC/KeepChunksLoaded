56cbb064d4c94216450d461faaab8c52fa01c6da
chris <acomputerdog@gmail.com>
2017-01-14 12:25:25 -0500

    Implement plugin

--- src/net/acomputerdog/keepchunksloaded/PluginKeepChunksLoaded.java
+++ src/net/acomputerdog/keepchunksloaded/PluginKeepChunksLoaded.java
@@ -1,4 +1,31 @@
 package net.acomputerdog.keepchunksloaded;
 
-public class PluginKeepChunksLoaded {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginKeepChunksLoaded extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        //register event
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        //unregister event to prevent duplicate firing after /reload
+        HandlerList.unregisterAll((JavaPlugin) this);
+    }
+
+    /**
+     * Traps and cancels the ChunkUnloadEvent.
+     *
+     * @param e Event object
+     */
+    @EventHandler
+    public void onChunkUnload(ChunkUnloadEvent e) {
+        e.setCancelled(true);
+    }
 }
